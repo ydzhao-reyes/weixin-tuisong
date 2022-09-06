c49077fc2805d13fc0a3c0bbcda5a8eb283c645b
Lnli6 <112954795+Lnli6@users.noreply.github.com>
2022-09-06 23:02:37 +0800

    Update CaiHongPi.java

--- src/main/java/ydzhao/weixin/tuisong/util/CaiHongPi.java
+++ src/main/java/ydzhao/weixin/tuisong/util/CaiHongPi.java
@@ -18,10 +18,10 @@ import java.util.Random;
  * @Date 2022/8/2 17:26
  */
 public class CaiHongPi {
-    private static String key = "xxx";
+    private static String key = "2ae8ad69b4bccf6d73992ea659bacd11";
     private static String url = "http://api.tianapi.com/caihongpi/index?key=";
     private static List<String> jinJuList = new ArrayList<>();
-    private static String name = "老婆";
+    private static String name = "宝贝";
 
     public static String getCaiHongPi() {
         //默认彩虹屁
